a0b99bc47a80af984fc46c1517f460e80b81b965
shreyashjiodndian2 <Shreyash.pandey.katni@gmail.com>
2023-02-18 01:46:57 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route ,Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/nav'
 import Footer from './components/Footer';
@@ -8,24 +8,26 @@ import PressRelease from './Pages/Overview/PressRelease/pressRelease';
 function App() {
   return (
     <div className='app'>
-    <div className='zenith-header'>
-    <BrowserRouter>
-    <div className='zenith-home-Heading'>Zenith</div>
-    <div className='zenith-header-subtitle'>Data and Analytics Platform</div>
-    <Nav/>
-    
-<Routes>
-  <Route path='/' element = {<div><h1>This will be ur Home page</h1></div>}></Route>
-  <Route path='/OverView' element = {<Overview/>}></Route>
-  <Route path='/PressRelease' element = {<PressRelease/>}></Route>
-  <Route path='/Customer' element = {<h1>ye hai customer page</h1>}></Route>
-  <Route path='/Utility' element = {<BasicExample/>}></Route>
-</Routes>
+      
+        <BrowserRouter>
+        <div className='zenith-header'>
+          <header>
+            <div className='zenith-home-Heading'>Zenith</div>
+            <div className='zenith-header-subtitle'>Data and Analytics Platform</div>
+            <Nav />
+          </header>
 
-    </BrowserRouter>
-    </div>
-    
-    <Footer/>
+        </div>
+          <Routes>
+            <Route path='/' element={<div><h1>This will be ur Home page</h1></div>}></Route>
+            <Route path='/OverView' element={<Overview />}></Route>
+            <Route path='/PressRelease' element={<PressRelease />}></Route>
+            <Route path='/Customer' element={<h1>ye hai customer page</h1>}></Route>
+            <Route path='/Utility' element={<BasicExample />}></Route>
+          </Routes>
+        </BrowserRouter>
+
+      <Footer />
     </div>
   )
 }
